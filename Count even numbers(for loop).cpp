6f76b19f76f46ec8bb2d9403529dd7155606ff9a
$C@M <sethagyeimensah2@gmail.com>
2024-06-04 09:45:40 +0000

    loop

---
+++ Count even numbers(for loop).cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int number;
+
+  cout<< "Enter the number of even numbers you want to print: ";
+
+  cin>>number;
+  int even =2;
+  for(int i = 0; i<number; i++){
+    cout<<even<<endl;
+    even = even + 2;
+    
+
+  }
+  
+  return 0;
+}
+
+
+
+
+
+
+
+//NOTES
+
+//for loop
+// #include <iostream>
+
+// int main() {
+
+
+//   int count = 0;
+  
+//   for(int i =0; i<10; i++){
+//     std::cout<< "number: "<< i <<std:: endl;
+//     count++;
+//     std::cout<< "count: "<< count << std::endl;
+
+//   }
+//   return 0;
+// }
+
+
+
+
+//while loop
+// #include <iostream>
+
+// int main(){
+
+//   int count = 0;
+  
+//   while (count <= 10){
+//     std::cout<< "number: "<< count <<std:: endl;
+//     count++;
+
+    
+//   }
+
+
+
+//   return 0;
+// }
+
+
+
+// DO while
+// #include <iostream>
+
+// int main(){
+
+//   int count = 0;
+
+  
+//   do{
+
+//     std::cout<< "number: "<< count <<std:: endl;
+//     count++;
+//   }while(count <= 10);
+
+  
+  
+//   return 0;
+// }
