e124b804cde465f233ada313ad0070e3990fd14c
$C@M <sethagyeimensah2@gmail.com>
2024-06-11 00:18:57 +0000

    Age by score

---
+++ Age_by_mark.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    int score;
+
+    cout<<"Enter your age: "<<endl;
+    cin>>age;
+
+    cout<<"Enter your score"<<endl;
+    cin>>score;
+
+    cout<<"The product of the two is "<<age*score<<endl;
+
+
+    return 0;
+}
